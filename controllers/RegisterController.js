e731d3bd7092267e691e170d5c4166d7aa7d4ce5
EugeniyBolotov <eugene.bolotov2@gmail.com>
2023-03-20 14:28:05 +0500

    asd
    asd

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -4,7 +4,7 @@ const db = require('../settings/config')
 exports.Registr = (req,res)=>{
         const {login,pwd} = req.query;
         if(login == undefined){
-            response.statusError(rows,res,400);
+            response.statusError(rows,res, 400);
         }
         else{
             db.query(`INSERT INTO bh8pzn9jfci7qpzb9jo2.Users (Email,Password) VALUES ('${login}', '${pwd}');`, (error,rows,fields) =>{
